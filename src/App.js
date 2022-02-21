4252e45b1460ca9b93ae91b7fd65078d2964466c
Ubuntu <ubuntu@ip-172-31-13-55.eu-central-1.compute.internal>
2022-02-21 20:53:44 +0000

    adding Figma components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import { MyCollection, NavBar, MarketingFooter } from './ui-components'
 
 function App() {
   return (
     <div className="App">
+      <NavBar width={"100vw"}/>
+      <MyCollection isPaginated itemsPerPage={3} />
+      <MarketingFooter width={"100vw"} />
     </div>
   );
 }
